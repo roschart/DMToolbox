f433e1d6d8ae8595f6a3139d885700d87baec3a1
roschart <jlbalirac@hotmail.com>
2024-02-11 12:38:32 +0000

    refactor: parametrize  calculate_attack_roll

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,18 +2,14 @@ import pytest
 
 from dmtoolbox.main import calculate_attack_roll
 
-def test_calculate_attack_roll_success_cases():
-    # CA, level, class, expected result
-    test_cases = [
-        (6, 0, "Any", 14),
-        (-4, 1, "Thief", 20),
-        (-4, 13, "Cleric", 17),
-        (9, 4, "Elf", 8),
-    ]
-
-    for ca, level, char_class, expected in test_cases:
-        assert calculate_attack_roll(ca, level, char_class) == expected
-
+@pytest.mark.parametrize("monster_ac, character_level, character_class, expected", [
+    (6, 0, "Any", 14),
+    (-4, 1, "Thief", 20),
+    (-4, 13, "Cleric", 17),
+    (9, 4, "Elf", 8),
+])
+def test_calculate_attack_roll_success_cases(monster_ac, character_level, character_class, expected):
+    assert calculate_attack_roll(monster_ac, character_level, character_class) == expected
 @pytest.mark.parametrize("monster_ac, character_level, character_class", [
     (-7, 1, "Thief"),  # CA fuera de rango
     (10, 1, "Cleric"),  # CA fuera de rango
