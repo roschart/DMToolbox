c13330aa699d0d943a185dd130413c472bad2416
roschart <jlbalirac@hotmail.com>
2024-02-11 12:29:47 +0000

    implement default case as Value Error

--- dmtoolbox/main.py
+++ dmtoolbox/main.py
@@ -9,4 +9,8 @@ CharacterClass = Literal[
 def calculate_attack_roll(
     monster_ac: int, character_level: int, character_class: CharacterClass
 ) -> int:
-    pass
+    match (monster_ac, character_level, character_class):
+        case (_, 0, _) :
+            return 0
+        case _:
+            raise ValueError("Calculated_attac_roll not implemented")
