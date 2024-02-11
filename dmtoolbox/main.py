a0818179fe7d7c995ebde6913f5be0ef3af92b83
roschart <jlbalirac@hotmail.com>
2024-02-11 17:20:45 +0000

    Pass all test

--- dmtoolbox/main.py
+++ dmtoolbox/main.py
@@ -2,7 +2,7 @@ from typing import Literal
 
 # Alias for the character class type
 CharacterClass = Literal[
-    "Cleric", "Elf", "Dwarf", "Ranger", "Warrior", "Thief", "Halfling"
+    "Cleric", "Elf", "Dwarf", "Ranger", "Warrior", "Thief", "Halfling", "Wizard"
 ]
 
 
@@ -10,14 +10,20 @@ def calculate_attack_roll(
     monster_ac: int, character_level: int, character_class: CharacterClass
 ) -> int:
     def calculate_row(character_level: int, character_class: CharacterClass) -> int:
+        limits:list[int]
         match character_class:
             case "Cleric" | "Thief":
                 limits = [3, 5, 8, 10, 11, 12, 14, 16, 18, 20]
-                for i, limit in enumerate(limits):
-                    if character_level <= limit:
-                        return i + 1
-                return len(limits)+1
-        return 0
+            case "Withard":
+                limits = [3, 7, 10, 12, 13, 15, 18, 20, 23, 24]
+            case _:
+                limits = [2,3,4,5,6,8,9,11,12,13,14,15,16,17,18]
+        for i, limit in enumerate(limits):
+            if character_level <= limit:
+                return i + 1
+        return len(limits) + 1
+
+    
 
     match (monster_ac, character_level, character_class):
         case (_, 0, _):
@@ -25,5 +31,9 @@ def calculate_attack_roll(
         case (_, _, "Cleric" | "Thief"):
             r = calculate_row(character_level, character_class)
             return max(2, min(20, 20 - monster_ac - r))
+        case (_,_, "Withard"):
+            r = calculate_row(character_level, character_class)
+            return max(2, min(20, 20 - monster_ac - r))
         case _:
-            raise ValueError("Calculated_attac_roll not implemented")
+            r = calculate_row(character_level, character_class)
+            return max(2, min(20, 20 - monster_ac - r))
